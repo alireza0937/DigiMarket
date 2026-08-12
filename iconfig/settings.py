86f43d0d4ebc48493831cb5d82e7c07c483e581f
Alireza <alireza.bayrami@gmail.com>
2026-08-12 20:58:36 +0330

    postgres config changed

--- iconfig/settings.py
+++ iconfig/settings.py
@@ -58,11 +58,11 @@ WSGI_APPLICATION = "iconfig.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("NAME"),
-        "USER": config("USER"),
-        "PASSWORD": config("PASSWORD"),
-        "HOST": config("HOST"),
-        "PORT": config("PORT"),
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
