13455f513ac159ebe938abdc93ec991ef8c26fb5
Alireza <alireza.bayrami@gmail.com>
2026-08-16 21:45:07 +0330

    ProductImage model created.

--- catalog/models.py
+++ catalog/models.py
@@ -38,3 +38,12 @@ class Product(models.Model):
         return self.name
     
     
+class ProductImage(models.Model):
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="products/")
+    is_main = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.product
+    
