a06ffd4660135b417602ca3b972ac545b92dd1ba
Alireza <alireza.bayrami@gmail.com>
2026-08-16 19:40:15 +0330

    Category model created.

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 
+class Category(models.Model):
     
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    parent = models.ForeignKey("self", null=True, blank=True, related_name="children")
+    
+    
+    def __str__(self):
+        return self.name
