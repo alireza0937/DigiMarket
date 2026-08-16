0784339cb339e80e6c96c5697fc7ed8c77f0b30a
Alireza <alireza.bayrami@gmail.com>
2026-08-16 21:39:09 +0330

    Tag model created & tags field added to Product model.

--- catalog/models.py
+++ catalog/models.py
@@ -13,6 +13,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Tag(models.Model):
+    
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
     
 class Product(models.Model):
     
@@ -23,10 +29,12 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=0)
     stock = models.PositiveIntegerField(default=0)
+    tags = models.ManyToManyField(Tag, related_name="products", blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
+    
     
