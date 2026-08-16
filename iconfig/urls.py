80b4873b6461fe5ed69a6605bad99c71dc680599
Alireza <alireza.bayrami@gmail.com>
2026-08-16 19:33:20 +0330

    The line spacing between the urls has changed.

--- iconfig/urls.py
+++ iconfig/urls.py
@@ -6,8 +6,14 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    
     path("admin/", admin.site.urls),
     path("account/", include("accounts.urls")),
+    path("category/", include("catalog.urls")),
+    
+    # SIMPLE JWT
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
