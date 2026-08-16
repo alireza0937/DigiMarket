7549ecd1e85ac7b822f1962676ba9de86c85b798
Alireza <alireza.bayrami@gmail.com>
2026-08-16 19:54:21 +0330

    Product model created.

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from accounts.models import SellerProfile
+
 
 class Category(models.Model):
     
@@ -10,3 +12,21 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Product(models.Model):
+    
+    seller = models.ForeignKey(SellerProfile, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=0)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
