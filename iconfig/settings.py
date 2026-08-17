6dc644ac30d9487e95a90274027549a76f095a23
Alireza <alireza.bayrami@gmail.com>
2026-08-17 21:58:55 +0330

    accounts & catalog APP registered on installed app

--- iconfig/settings.py
+++ iconfig/settings.py
@@ -23,7 +23,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # MY APPS
+    
     "accounts",
+    "catalog",
 ]
 
 MIDDLEWARE = [
