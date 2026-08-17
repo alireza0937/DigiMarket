926e05ab8fc7db8dbb99931fe40ffc28b0f87c6c
Alireza <alireza.bayrami@gmail.com>
2026-08-17 21:46:06 +0330

    registration view added using generics

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import RegisterCreateView
 
 urlpatterns = [
-    
+    path("api/register/", RegisterCreateView.as_view(), name="register-api-view")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from accounts.models import User
+from accounts.serializer import RegisterSerializer
 
-# Create your views here.
+class RegisterCreateView(generics.CreateAPIView):
+    
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+    
