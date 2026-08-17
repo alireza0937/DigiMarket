725d61f56b3904f3d50eb598c4d05c3156c883cf
Alireza <alireza.bayrami@gmail.com>
2026-08-17 22:24:51 +0330

    all the models in catalog APP registered

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, SellerProfile, Tag
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(SellerProfile)
+admin.site.register(Tag)
